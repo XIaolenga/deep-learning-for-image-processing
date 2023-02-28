35a8722f26cc3e2f68148fcf30e78ccb57906aa4
XIaolenga <112293211+XIaolenga@users.noreply.github.com>
2023-02-28 14:58:29 +0800

    Update train.py

--- pytorch_classification/Test2_alexnet/train.py
+++ pytorch_classification/Test2_alexnet/train.py
@@ -29,28 +29,28 @@ def main():
     data_root = os.path.abspath(os.path.join(os.getcwd(), "../.."))  # get data root path
     image_path = os.path.join(data_root, "data_set", "flower_data")  # flower data set path
     assert os.path.exists(image_path), "{} path does not exist.".format(image_path)
-    train_dataset = datasets.ImageFolder(root=os.path.join(image_path, "train"),
+    train_dataset = datasets.ImageFolder(root=os.path.join(image_path, "train"),      #加载训练数据集
                                          transform=data_transform["train"])
-    train_num = len(train_dataset)
+    train_num = len(train_dataset)      #统计训练数据个数
 
     # {'daisy':0, 'dandelion':1, 'roses':2, 'sunflower':3, 'tulips':4}
     flower_list = train_dataset.class_to_idx
-    cla_dict = dict((val, key) for key, val in flower_list.items())
+    cla_dict = dict((val, key) for key, val in flower_list.items())  #把字典里的（key, vlaue）转换成（vlaue, key）
     # write dict into json file
     json_str = json.dumps(cla_dict, indent=4)
-    with open('class_indices.json', 'w') as json_file:
-        json_file.write(json_str)
+    with open('class_indices.json', 'w') as json_file:    
+        json_file.write(json_str)        #将改好的类别字典放入.json文件中
 
     batch_size = 32
-    nw = min([os.cpu_count(), batch_size if batch_size > 1 else 0, 8])  # number of workers
-    print('Using {} dataloader workers every process'.format(nw))
+    #nw = min([os.cpu_count(), batch_size if batch_size > 1 else 0, 8])  # number of workers
+    #print('Using {} dataloader workers every process'.format(nw))
 
     train_loader = torch.utils.data.DataLoader(train_dataset,
                                                batch_size=batch_size, shuffle=True,
-                                               num_workers=nw)
+                                               num_workers=0)             #加载训练数据集，并进行预处理
 
     validate_dataset = datasets.ImageFolder(root=os.path.join(image_path, "val"),
-                                            transform=data_transform["val"])
+                                            transform=data_transform["val"]) #加载测试数据集
     val_num = len(validate_dataset)
     validate_loader = torch.utils.data.DataLoader(validate_dataset,
                                                   batch_size=4, shuffle=False,
@@ -70,12 +70,12 @@ def main():
     # print(' '.join('%5s' % cla_dict[test_label[j].item()] for j in range(4)))
     # imshow(utils.make_grid(test_image))
 
-    net = AlexNet(num_classes=5, init_weights=True)
+    net = AlexNet(num_classes=5, init_weights=True)   #实例化网络
 
     net.to(device)
-    loss_function = nn.CrossEntropyLoss()
+    loss_function = nn.CrossEntropyLoss()#计算交叉熵损失
     # pata = list(net.parameters())
-    optimizer = optim.Adam(net.parameters(), lr=0.0002)
+    optimizer = optim.Adam(net.parameters(), lr=0.0002)  #优化器 Adam
 
     epochs = 10
     save_path = './AlexNet.pth'
@@ -83,42 +83,42 @@ def main():
     train_steps = len(train_loader)
     for epoch in range(epochs):
         # train
-        net.train()
+        net.train()    #为了防止dropout印象模型参数，所以采用net.train()和net.eval()方法
         running_loss = 0.0
         train_bar = tqdm(train_loader, file=sys.stdout)
-        for step, data in enumerate(train_bar):
-            images, labels = data
-            optimizer.zero_grad()
-            outputs = net(images.to(device))
-            loss = loss_function(outputs, labels.to(device))
-            loss.backward()
-            optimizer.step()
+        for step, data in enumerate(train_bar):       #遍历训练集中的数据和步骤
+            images, labels = data                   #将数据分为 图片和标签
+            optimizer.zero_grad()                   #梯度置零
+            outputs = net(images.to(device))        #将图像放入GPU后输出
+            loss = loss_function(outputs, labels.to(device))#计算输出与真实标签的损失
+            loss.backward()                               #损失的反向传播
+            optimizer.step()                            #迭代优化器
 
             # print statistics
-            running_loss += loss.item()
+            running_loss += loss.item()        #计算损失和
 
             train_bar.desc = "train epoch[{}/{}] loss:{:.3f}".format(epoch + 1,
                                                                      epochs,
                                                                      loss)
-
+#打印训练损失
         # validate
         net.eval()
         acc = 0.0  # accumulate accurate number / epoch
-        with torch.no_grad():
+        with torch.no_grad():                       ##验证过程保持不设置梯度，保持训练参数不变
             val_bar = tqdm(validate_loader, file=sys.stdout)
-            for val_data in val_bar:
+            for val_data in val_bar:           #加载遍历验证数据集
                 val_images, val_labels = val_data
                 outputs = net(val_images.to(device))
-                predict_y = torch.max(outputs, dim=1)[1]
-                acc += torch.eq(predict_y, val_labels.to(device)).sum().item()
+                predict_y = torch.max(outputs, dim=1)[1]  # 输出预测值的标签
+                acc += torch.eq(predict_y, val_labels.to(device)).sum().item()   #统计正确的个数
 
-        val_accurate = acc / val_num
+        val_accurate = acc / val_num                    #计算精度
         print('[epoch %d] train_loss: %.3f  val_accuracy: %.3f' %
               (epoch + 1, running_loss / train_steps, val_accurate))
 
         if val_accurate > best_acc:
             best_acc = val_accurate
-            torch.save(net.state_dict(), save_path)
+            torch.save(net.state_dict(), save_path)  #保存最好的精度时的网络参数模型
 
     print('Finished Training')
 
