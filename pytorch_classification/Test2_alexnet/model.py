0fe3d2c6f31bfd2baf8bba49fc1e8af722c3bf53
XIaolenga <112293211+XIaolenga@users.noreply.github.com>
2023-02-28 12:38:31 +0800

    Update model.py

--- pytorch_classification/Test2_alexnet/model.py
+++ pytorch_classification/Test2_alexnet/model.py
@@ -6,7 +6,7 @@ class AlexNet(nn.Module):#hhh
     def __init__(self, num_classes=1000, init_weights=False):
         super(AlexNet, self).__init__()
         self.features = nn.Sequential(
-            nn.Conv2d(3, 48, kernel_size=11, stride=4, padding=2),  # input[3, 224, 224]  output[48, 55, 55]
+            nn.Conv2d(3, 48, kernel_size=11, stride=4, padding=2),  # input[3, 224, 224]  output[48, 55, 55]  54.25向上取整
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=3, stride=2),                  # output[48, 27, 27]
             nn.Conv2d(48, 128, kernel_size=5, padding=2),           # output[128, 27, 27]
